fa049c5a0a5a1c1cc1fb9c1528d31d6ce5eb912b
yusrilizzhap <yusrilizzha01@gmail.com>
2020-04-25 21:18:36 +0700

    tugas2LINIERLAYOUT

---
+++ app/src/main/java/com/example/firebass/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firebass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/firebass/auth.kt
@@ -0,0 +1,12 @@
+package com.example.firebass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class auth : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
